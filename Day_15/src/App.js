f71f794e8445116ad88bd0b47fa93179b8bfa533
Abhishek Chauhan <abhishekchauhan1509@gamil.com>
2021-07-10 19:40:41 +0530

    Letter with css

---
+++ Day_15/src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello World </h1>
+      <h2>Start React today!</h2>
+    </div>
+  );
+}
