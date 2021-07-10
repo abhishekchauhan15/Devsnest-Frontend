c006d8f581b12c8895ac7f3944174a1115a7be69
Abhishek Chauhan <abhishekchauhan1509@gamil.com>
2021-07-10 21:18:41 +0530

    final

---
+++ Day_17/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./components/header"
+import List from "./components/list"
+
+function App() {
+  return(
+  <>
+  <div className="main">
+   <Header/>
+    <List/>
+  </div>
+  </>
+  )
+}
+
+export default App;
