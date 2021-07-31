6580cc9566d4147da0ad3a4a0cf78d7b618166d4
Abhishek Chauhan <abhishekchauhan1509@gmail.com>
2021-07-31 12:41:37 +0530

    deploy done

---
+++ Day_21/src/Components/CaloriePage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function CaloriePage() {
+    return (
+        <div>
+        <forms>
+            <input type="text" placeholder="Item Name"/>
+            <input type="text" placeholder="Calorie Amount"/>
+            <button>Add Item</button>
+        </forms>
+        
+            
+        </div>
+    )
+}
+
+export default CaloriePage
