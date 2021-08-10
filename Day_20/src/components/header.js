8ac1c6191770281fcfc802d9ab99aaf73bc087c6
Abhishek Chauhan <abhishekchauhan1509@gmail.com>
2021-08-10 23:03:07 +0530

    added build

--- Day_20/src/components/header.js
+++ Day_20/src/components/header.js
@@ -4,7 +4,7 @@ import "../styles/header.css";
 function Header(){
     return(
         <div className="header">
-            <h1>This is a Calorie App</h1>
+            <h1>A Simple Calorie Tracker App</h1>
         </div>
     )
 }
