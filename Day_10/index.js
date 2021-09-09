ca63270d34dc9e9bc3506d1234cd87b9a8cc954f
Abhishek Chauhan <abhishekchauhan1509@gmail.com>
2021-09-09 23:55:21 +0530

    meme app updated

--- Day_10/index.js
+++ Day_10/index.js
@@ -1,7 +1,5 @@
-let words=["1", "2", "3", "4", "5", "6", "7", "8"];
-
-
-words=[...words, ...words];
+let photos=["https://bit.ly/3nfdDh4","https://bit.ly/2X0XvEI","https://bit.ly/3l4SJP1","https://bit.ly/2VpyjaB","https://bit.ly/3tqAQOc","https://bit.ly/38UoMLG","https://bit.ly/2Yrvl6z","https://bit.ly/3yTnStF"];
+photos=[...photos, ...photos];
 let moves=30;
 let matches=0;
 let gameActive=true;
@@ -9,60 +7,53 @@ let gameActive=true;
 for(let i=1; i<=16; i++){
     let card=document.createElement("div");
     card.classList.add("flip-card");
-
     let inner=document.createElement("div");
     inner.classList.add("flip-inner");
-
     let front=document.createElement("div");
     front.classList.add("front");
-
     let back=document.createElement("div");
-
     let rand=Math.floor(Math.random()*(16-i));
-
-    back.innerHTML="<h2>"+words[rand]+"</h2>";
-    words.splice(rand, 1);
-
+    // back.innerHTML="<h2>"+photos[rand]+"</h2>";
+    back.innerHTML="<img class='image' src="+photos[rand]+"></img>";
+    photos.splice(rand, 1);
     back.classList.add("back");
-
     inner.appendChild(front);
     inner.appendChild(back);
-
     card.appendChild(inner);
     document.getElementById("grid").appendChild(card);
 }
 
-let clicked;
-let cards=document.getElementsByClassName("flip-card");
+let click;
+let btns=document.getElementsByClassName("flip-card");
 
-for(let card of cards){
-    card.addEventListener("click", (e)=>{
+for(let i=0; i<16; i++){
+    btns[i].addEventListener("click", (e)=>{
         if(!gameActive)return;
         moves--;
         document.querySelector("span").innerHTML=moves;
-        
-        if(clicked){
-            card.firstChild.classList.add("flipped");
+        if(click){
+            btns[i].firstChild.classList.add("flipped");
             gameActive=false;
             setTimeout(() => {
-                if(card.firstChild.lastChild.innerHTML !== clicked.firstChild.lastChild.innerHTML){
-                    card.firstChild.classList.remove("flipped");
-                    clicked.firstChild.classList.remove("flipped");
+                if(!(btns[i].firstChild.lastChild.innerHTML === click.firstChild.lastChild.innerHTML)){
+                    btns[i].firstChild.classList.remove("flipped");
+                    click.firstChild.classList.remove("flipped");
                 }else{
                     matches++;
-                    card.style.visibility="hidden";
-                    clicked.style.visibility="hidden";
+                    let cloneBtn=btns[i].cloneNode(true);
+                    btns[i].parentNode.replaceChild(cloneBtn, btns[i]);
+                    let cloneClick=click.cloneNode(true);
+                    click.parentNode.replaceChild(cloneClick, click);
                 }
-                clicked=undefined;
+                click=undefined;
                 gameActive=true;
             }, 500);
         }else{
-            clicked=card;
-            card.firstChild.classList.add("flipped");
+            click=btns[i];
+            btns[i].firstChild.classList.add("flipped");
         }
     })
 }
-
 setInterval(() => {
     if(moves === 0){
         document.querySelector(".popup").style.display="flex";
--- Day_22/src/components/template.jsx
+++ Day_22/src/components/template.jsx
@@ -1,24 +1,30 @@
 import React from "react";
+import { Button } from "@material-ui/core";
 
-const Template = ({ templates, setMeme }) => {
-    return (
-                   
-        <div className="template">
-            {templates.map((template) => (
-                <div
-                    key={template.id}
-                    onClick={() => {
-                        setMeme(template);
-                    }}
-                >
-                    <div
-                        style={{ backgroundImage: `url(${template.url})` }}
-                        className="image"
-                    ></div>
-                </div>
-            ))}
-        </div>
-    );
-};
-
-export default Template;
+export default function MemeGenerator_gallery(props) {
+  const { templates, setMeme } = props;
+  return (
+    <>
+      <div className="memeGenerator_gallery">
+        {templates.map((template) => (
+          <div key={template.id} className="meme_Template">
+            <img loading="lazy" src={template.url} alt="MemeImg" />
+            <Button
+              variant="outlined"
+              color="primary"
+              onClick={(e) => {
+                //   console.log(template);
+                setMeme(template);
+                // e.target.parentNode.parentNode.firstChild.src;
+              }}
+            >
+              {template.name}
+            </Button>
+          </div>
+        ))}
+        ;
+      </div>
+      ;
+    </>
+  );
+}
